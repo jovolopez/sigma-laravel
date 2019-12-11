b59ee4a78f3a0aba46df60e0d744cb5e386b5caf
DigitalHouse <you@example.com>
2019-12-11 16:34:44 -0300

    JS fix

--- public/js/validation-login.js
+++ public/js/validation-login.js
@@ -3,7 +3,6 @@ window.onload = function(){
   var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   var formulario = document.querySelector('.login-form');
   formulario.onsubmit = function (evento) {
-    evento.preventDefault();
     console.log(formulario)
   }
 
