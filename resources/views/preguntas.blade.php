9f5b0d0a36bf35a9cd06a2f06327ef69337f2db8
Eliecer Gonzalez <eliecerega@gmail.com>
2019-12-19 16:40:30 -0300

    nuevas preguntas

--- resources/views/preguntas.blade.php
+++ resources/views/preguntas.blade.php
@@ -17,15 +17,23 @@ FAQ
                     <div class="panel">
                       <p>Los cambios se pueden realizar hasta 30 dias despues de realizada la compra.</p>
                     </div>
-                    <button class="accordion">¿Que debo hacer si llega algun producto equivocado o en mal estado?</button>
+                  <button class="accordion">¿Que debo hacer si llega algun producto equivocado o en mal estado?</button>
+                    <div class="panel">
+                      <p>Te pones en contacto con nosotros y te damos una solición.</p>
+                    </div>
+                  <button class="accordion">¿Como puedo consultar el estado de mi pedido?</button>
                       <div class="panel">
-                        <p>Te pones en contacto con nosotros y te damos una solición.</p>
+                        <p>En tu perfil encontraras una opción que te permitira ver el estado de tu pedido.</p>
                       </div>
-                      <button class="accordion">¿Como puedo consultar el estado de mi pedido?</button>
-                        <div class="panel">
-                          <p>En tu perfil de usuario encontraras unas opción que te permitira ver el estado de tu pedido.</p>
-                        </div>
-                    </ul>
+                  <button class="accordion">¿Cuales son los metodos de pago?</button>
+                      <div class="panel">
+                        <p>Tenemos MercadoPago y PayPal</p>
+                      </div>
+                  <button class="accordion">¿Cuanto tarda en llegar mi pedido?</button>
+                      <div class="panel">
+                        <p>La entrega se tarda de 4 dias a una semana</p>
+                      </div>
+                </ul>
                     <hr size="1px" color="#ddd">
                     <a href="contacto">¿No encontró lo que buscaba? Contacte con nosotros <b>aquí!</b></a>
               </div>
