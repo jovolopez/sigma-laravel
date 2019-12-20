5edd38bcc81b925bbf6bc2708876bf7fc678656a
Javveru <thezepols@gmail.com>
2019-12-20 04:07:30 -0300

    CSS de bootstrap menos pisado. (1 linea)

--- resources/views/cargarProducto.blade.php
+++ resources/views/cargarProducto.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 @section('css')
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/editarProducto.css">
 @endsection
 @section('title')
 Cargar Producto
